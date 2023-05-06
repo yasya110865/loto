56ebba30708faba5519a87d58eebaf1139002bfd
yasya110865 <evn04@mail.ru>
2023-05-06 19:04:25 +0600

    loto classes and tests

--- lotofunctions3.py
+++ lotofunctions3.py
@@ -1,33 +1,21 @@
 import random
 
 class Card:
-    def __init__(self):
-        # self.row = self.create_row()
-        self.card = self.create_card()
-
-    def create_row(self):
-        num = list(range(1, 91))
-        numbers = sorted(random.sample(num,9))
-        idx = random.sample(range(0, 8), 4)
-        for i in idx:
-            numbers[i] = ' '
-        num = list(set(num) - set(numbers))
-        return numbers, num
+    # def __init__(self):
+    #     self.card = self.create_card()
 
     def create_card(self):
+        num = list(range(1, 91))
         card = []
-
         for i in range(3):
-            row, num = self.create_row()
-            card.append(row)
-        # self.my_card = card
+            numbers = sorted(random.sample(num, 9))
+            idx = random.sample(range(0, 8), 4)
+            for i in idx:
+                numbers[i] = ' '
+            num = list(set(num) - set(numbers))
+
+            card.append(numbers)
         return card
-    def print_card(self):
-        print('-' * 23)
-        print(' '.join(map(str, self.card[0])))
-        print(' '.join(map(str, self.card[1])))
-        print(' '.join(map(str, self.card[2])))
-        print('-' * 23)
 
 
 class Person:
@@ -39,48 +27,29 @@ class Person:
         self.name = 'Name'
         card = Card()
         self.card = card.create_card()
-    # def create_row(self):
-    #     num = list(range(1, 91))
-    #     numbers = sorted(random.sample(num,9))
-    #     idx = random.sample(range(0, 8), 4)
-    #     for i in idx:
-    #         numbers[i] = ' '
-    #     num = list(set(num) - set(numbers))
-    #     return numbers, num
-    #
-    # def create_card(self):
-    #     card = []
-    #
-    #     for i in range(3):
-    #         row, num = self.create_row()
-    #         card.append(row)
-    #     self.my_card = card
-    #     return card
-    # def print_card(self):
-    #     print('-' * 23)
-    #     print(' '.join(map(str, self.my_card[0])))
-    #     print(' '.join(map(str, self.my_card[1])))
-    #     print(' '.join(map(str, self.my_card[2])))
-    #     print('-' * 23)
+
+    def print_card(self):
+        print('-' * 23)
+        print(' '.join(map(str, self.card[0])))
+        print(' '.join(map(str, self.card[1])))
+        print(' '.join(map(str, self.card[2])))
+        print('-' * 23)
 
 
     def delete(self,n,actions):
         '''
         зачеркивание числа, совпадающего с заданным
         :param n: заданное число
-        :param card: списки чисел с карточки
+        :param actions: решение игрока (y/n)
         :return:
         '''
-        # card = self.my_card
+
         if actions == 'y' and any(n in sl for sl in self.card):
             res = 0
             for i in range(3):
                 if n in self.card[i]:
                     self.card[i][self.card[i].index(n)] = '-'
                     self.count += 1
-
-
-
 
         elif actions == 'n' and not any(n in sl for sl in self.card):
             res = 0
@@ -107,6 +76,22 @@ class Game:
         self.names = []
         self.bag = random.sample(list(range(1, 91)), 90)
 
+    def get_random_num(self):
+        # случайный бочонок
+        n = random.choice(self.bag)
+
+        # убираем из мешка
+        self.bag.remove(n)
+        return n
+
+    def game_over(self, playerscard, failed, numplayers):
+        if len(set(playerscard[0] + playerscard[1] + playerscard[2])) < 3:
+            result = 'Gameover_vinner'
+        elif len(failed) == numplayers:
+            result = 'Gameover'
+        else:
+            result = 'Continue'
+        return result
     def game_start(self):
         self.num_players = int(input('Сколько будет игроков? '))
         print('1. Человек')
@@ -122,10 +107,8 @@ class Game:
             self.names.append(name)
         while True:
             # случайный бочонок
-            n = random.choice(self.bag)
+            n = self.get_random_num()
 
-            # убираем из мешка
-            self.bag.remove(n)
             print(f'Новый бочонок {n}, осталось {len(self.bag)}')
 
             # печатаем карточки игрокам
@@ -134,8 +117,8 @@ class Game:
                 if self.players[i] not in self.failed:
 
                     print(f'Карта игрока {self.names[i]},число {n} ')
-                    # card = self.players[i].create_card()
-                    self.players[i].card.print_card()
+
+                    self.players[i].print_card()
                     if isinstance(self.players[i], Computer):
                         self.players[i].delete(n)
 
@@ -147,18 +130,29 @@ class Game:
                             self.failed.append(self.players[i])
                         print(f'Счет игрока {self.players[i].count}')
 
-            # if len(self.failed) == num_players:
-            #     print('Игра окончена!')
-            #     break
-                # print(len(set(cards[i][0] + cards[i][1] + cards[i][2])))
-            if (len(set(self.players[i].card[0] + self.players[i].card[1] + self.players[i].card[2]))) < 3:
+            result = self.game_over(self.players[i].card, self.failed,self.num_players)
+            if result == 'Gameover_vinner':
+
                 print(f'Игра окончена! Победил игрок {self.names[i]}')
                 break
-            if len(self.failed) == self.num_players:
+            elif result == 'Gameover':
+
                 print('Игра окончена!')
                 break
 
 
 
 if __name__ == '__main__':
-    pass
+    game = Game()
+    game.game_start()
+
+
+
+
+
+
+
+
+
+
+
